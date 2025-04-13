018078f40ac2ad5ac6312f3fa6308a92d2213d5e
Saviskar <Saviskar>
2025-04-13 23:39:03 +0530

    added createResidents option in controller

--- controllers/ResidentController.js
+++ controllers/ResidentController.js
@@ -9,4 +9,32 @@ module.exports = {
       return res.json(results);
     });
   },
+
+  createResidents: (req, res) => {
+    const sql = `
+    INSERT INTO residents (
+      surname, givenName, preferredNames, age,
+      maritalStatus, telephone, postCode,
+      religion, countryOfBirth, preferredLanguage
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+    const values = [
+      req.body.surname,
+      req.body.givenName,
+      req.body.preferredNames,
+      req.body.age,
+      req.body.maritalStatus,
+      req.body.telephone,
+      req.body.postCode,
+      req.body.religion,
+      req.body.countryOfBirth,
+      req.body.preferredLanguage,
+    ];
+
+    con.query(sql, values, (err, results) => {
+      if (err) return res.status(500).json(err);
+      return res.status(201).json(results);
+    });
+  },
 };
