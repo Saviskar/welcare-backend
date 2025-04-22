2358ce46fd59e7b160ab788f94ffe628ccac217a
Saviskar <Saviskar>
2025-04-22 13:53:00 +0530

    refactoring

--- routes/User.js
+++ routes/User.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import { getAllUsers,login} from '../controllers/UserController.js';
+import express from "express";
+import { getAllUsers, login } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 //login
-router.post('/login',login)
+router.post("/login", login);
 
 //logout
 
-router.get('/user', getAllUsers);
+router.get("/", getAllUsers);
 
 export default router;
